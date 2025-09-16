6ce4e1336b4dc9b994890d2f160d11a87b53b937
sleeping-code <frangelicamuaya16@gmail.com>
2025-09-16 03:57:24 -0500

    test: Update user name in DatabaseSeeder

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'User',
             'email' => 'test@example.com',
         ]);
     }
